56a56883a402b7f84ceedfaf14ce8c294b722d33
nyankno <118753747+nyankno@users.noreply.github.com>
2023-05-23 14:41:10 +0100

    add file

---
+++ index.js
@@ -0,0 +1,48 @@
+
+
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+/*==================scroll section active link =================*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        let html = sec.getAttribute('html');
+
+        if (top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id +  html +']').classList.add('active')
+            });
+        };
+    });
+}
+
+
+var swiper = new Swiper(".home", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    //navigation: {
+      //nextEl: ".swiper-button-next",
+      //prevEl: ".swiper-button-prev"
+   // },
+});
